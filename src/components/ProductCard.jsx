d57be3d6cf26139910bcb43e459ddf19725d3bfe
TarasDemchenko <tarasdemchenko1993@gmail.com>
2024-10-24 16:51:27 +0200

    product curd

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,15 @@
 import { FaStar } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../redux/cartSlice';
 
 const ProductCard = ({ product }) => {
+  const dispath = useDispatch();
+  const handleAddToCart = (e, product) => {
+    e.stopPropagation();
+    e.preventDefault();
+    dispath(addToCart(product));
+    alert('Product Added Succesfully!');
+  };
   return (
     <div className=" bg-white shadow rounded relative border transform transition-transform duration-300 hover:scale-105">
       <img src={product.image} className="w-full h-48 object-contain mb-4" />
@@ -12,7 +21,10 @@ const ProductCard = ({ product }) => {
         <FaStar className="text-yellow-500" />
         <FaStar className="text-yellow-500" />
       </div>
-      <div className="absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-amber-600 group text-white text-sm rounded-full hover:w-32 hover:bg-amber-700 transition-all ">
+      <div
+        className="absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-amber-600 group text-white text-sm rounded-full hover:w-32 hover:bg-amber-700 transition-all "
+        onClick={e => handleAddToCart(e, product)}
+      >
         <span className="group-hover:hidden">+</span>
         <span className="hidden group-hover:block">Add to cart</span>
       </div>
